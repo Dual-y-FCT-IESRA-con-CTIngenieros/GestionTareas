07ea61d58c634f082d643b168c23e24d2621808d
ilopjim13 <ljivan@iesrafaelalberti.es>
2025-04-02 13:13:53 +0200

    añadido voyager y creado LoginScreen

--- AppMovil/composeApp/build.gradle.kts
+++ AppMovil/composeApp/build.gradle.kts
@@ -43,6 +43,8 @@ kotlin {
             implementation(compose.components.uiToolingPreview)
             implementation(libs.androidx.lifecycle.viewmodel)
             implementation(libs.androidx.lifecycle.runtime.compose)
+            implementation(libs.voyager.navigator)
+            implementation(libs.voyager.transitions)
         }
     }
 }
---
+++ AppMovil/composeApp/src/commonMain/kotlin/com/es/appmovil/screens/LoginScreen.kt
@@ -0,0 +1,7 @@
+package com.es.appmovil.screens
+
+/**
+ * Pantalla del login que permite iniciar sesion a los trabajadores.
+ */
+class LoginScreen {
+}
