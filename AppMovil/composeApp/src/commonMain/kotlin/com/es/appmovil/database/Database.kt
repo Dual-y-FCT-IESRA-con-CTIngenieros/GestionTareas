4cd1ea7d74074af7c492a956d66cf31946f3a42a
ilopjim13 <ljivan@iesrafaelalberti.es>
2025-04-02 13:01:11 +0200

    Estructura principal de carpetas creado

---
+++ AppMovil/composeApp/src/commonMain/kotlin/com/es/appmovil/database/Database.kt
@@ -0,0 +1,7 @@
+package com.es.appmovil.database
+
+/**
+ * Singelton con la conexión ha la base de datos supabase que gestiona los datos
+ */
+object Database {
+}
---
+++ AppMovil/composeApp/src/commonMain/kotlin/com/es/appmovil/viewmodel/UserViewmodel.kt
@@ -0,0 +1,7 @@
+package com.es.appmovil.viewmodel
+
+/**
+ * Clase viewmodel para el usuario, donde guardaremos los datos del usuario y sus posibles funciones
+ */
+class UserViewmodel {
+}
