d618b5d4f1d316a433d73540df24fe268e328859
ilopjim13 <ljivan@iesrafaelalberti.es>
2025-04-21 13:41:35 +0200

    push para ver si funciona

---
+++ AppMovil/composeApp/src/commonMain/kotlin/com/es/appmovil/screens/TableManageScreen.kt
@@ -0,0 +1,30 @@
+package com.es.appmovil.screens
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import cafe.adriel.voyager.core.screen.Screen
+
+class TableManageScreen : Screen {
+    @Composable
+    override fun Content() {
+        Column(Modifier.fillMaxSize()) {
+            Text("Gestión Tablas")
+            val listaTablas = mutableListOf<String>()
+            LazyColumn {
+                items(listaTablas.size) {
+                    TableData()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun TableData() {
+    Row {  }
+}
