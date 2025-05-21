1a477d5641932c0a696bfc0587d1e96ee7b56760
ivanjim13 <ivangorrion57@gmail.com>
2025-05-21 13:09:07 +0200

    gestionar calendario mejorado, se puede bloquear o desbloquear a personas en especifico, se pueden filtrar y ordenar, hay muestra visual de los bloqueados y blockDate agregada a Employee y EmployeeUpdateDTO, y errores que corregir en la unión

--- AppMovil/composeApp/src/commonMain/kotlin/com/es/appmovil/screens/CalendarManageScreen.kt
+++ AppMovil/composeApp/src/commonMain/kotlin/com/es/appmovil/screens/CalendarManageScreen.kt
@@ -10,7 +10,6 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Button
@@ -27,7 +26,6 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.icons.filled.ArrowDownward
-import androidx.compose.material.icons.filled.ArrowOutward
 import androidx.compose.material.icons.filled.ArrowUpward
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.runtime.Composable
