e0e9831d737b7aeff12c80f4a1a5fa09ecec10f2
ivanjim13 <ivangorrion57@gmail.com>
2025-05-28 12:20:22 +0200

    Intento de filtrar el activity fracasado...

--- AppMovil/composeApp/src/commonMain/kotlin/com/es/appmovil/widgets/DayMenu.kt
+++ AppMovil/composeApp/src/commonMain/kotlin/com/es/appmovil/widgets/DayMenu.kt
@@ -27,19 +27,14 @@ import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.focus.FocusRequester
-import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import com.es.appmovil.model.EmployeeActivity
@@ -49,7 +44,6 @@ import com.es.appmovil.viewmodel.CalendarViewModel
 import com.es.appmovil.viewmodel.DataViewModel.activities
 import com.es.appmovil.viewmodel.DataViewModel.employee
 import com.es.appmovil.viewmodel.DayMenuViewModel
-import kotlinx.coroutines.launch
 import kotlinx.datetime.LocalDate
 
 
